64010c0f25bf4672543debbd2e35a7b8ccc43aa6
Sebastia <sebasvinaros@gmail.com>
2018-05-14 16:03:49 +0200

    fixed bug printing

--- queue/README.md
+++ queue/README.md
@@ -1,1 +1,12 @@
 ## Queue Data Structure
+
+A C structure modeling a FIFO queue of integers. The basic structure is:
+
+```c
+struct Queue{
+    int front,rear,capacity;
+    int* array;
+    int size;
+};
+```
+We must define two functions, enqueue and dequeue. The first one is used to add new items to the list whilst the second to remove the items in order in the list. Rear and front indicate the tail and the front of the list, respectively. 
--- queue/queue.c
+++ queue/queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 struct Queue{
     int front,rear,capacity;
@@ -8,9 +9,9 @@ struct Queue{
 };
 
 struct Queue* create_queue(int capacity){
-    struct Queue* queue = (struct Queue*)malloc(capacity*sizeof(struct Queue));
+    struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
     (*queue).front = 0; //index to remove items
-    (*queue).rear = 0; //index to add items
+    (*queue).rear = capacity -1;
     (*queue).capacity = capacity;
     (*queue).size = 0;
     (*queue).array = (int*)malloc(capacity*sizeof(int));
@@ -18,28 +19,39 @@ struct Queue* create_queue(int capacity){
     return queue;
 }
 
-int pop_item_queue(struct Queue* queue){
-    if((*queue).size > 0){
-        int item = (*queue).array[(*queue).front];
-        (*queue).front = ((*queue).front + 1)%(*queue).capacity;
-        (*queue).size -=1;
-        return item;
-    }
-    else{
-        return 0;
-    }
+int IsEmpty(struct Queue* queue){
+    return (*queue).size == 0;
+}
+int IsFull(struct Queue* queue){
+    return (*queue).size == (*queue).capacity;
 }
 
-void add_item_queue(struct Queue* queue, int item){
-    if((*queue).size < (*queue).capacity){
-        (*queue).array[(*queue).rear] = item;
-        (*queue).size +=1;
-        (*queue).rear = ((*queue).rear + 1)%(*queue).capacity;
+int Dequeue(struct Queue* queue){
+    if(IsEmpty(queue)){
+        return INT_MAX;
     }
+
+    int item = (*queue).array[(*queue).front];
+    (*queue).front = ((*queue).front + 1)%(*queue).capacity;
+    (*queue).size -=1;
+    return item;
 }
 
-void print_queue(struct Queue* queue){
-    for(int i = (*queue).front; i < (*queue).rear; i++){
+void Enqueue(struct Queue* queue, int item){
+    if(IsFull(queue)){
+        printf("blaaaa");
+        return;
+    }
+    //printf("item %d,%d,%d\n",item,(*queue).size,(*queue).rear);
+    (*queue).size +=1;
+    (*queue).rear = ((*queue).rear + 1)%(*queue).capacity;
+    (*queue).array[(*queue).rear] = item;
+    //printf("item %d,%d,%d\n",item,(*queue).size,(*queue).rear);
+    //printf("%d enqueued to queue\n", item);
+}
+
+void PrintQueue(struct Queue* queue){
+    for(int i = (*queue).front; i < (*queue).rear+1; i++){
         printf("%d\n", (*queue).array[i]);
     }
 }
@@ -47,17 +59,23 @@ void print_queue(struct Queue* queue){
 int main(){
 
     struct Queue* queue = create_queue(7);
-    add_item_queue(queue,0);
-    add_item_queue(queue,1);
-    add_item_queue(queue,2);
-    add_item_queue(queue,3);
-    add_item_queue(queue,4);
-    add_item_queue(queue,5);
-    print_queue(queue);
+    Enqueue(queue,0);
+    Enqueue(queue,1);
+    Enqueue(queue,2);
+    Enqueue(queue,3);
+    Enqueue(queue,4);
+    Enqueue(queue,5);
+    Enqueue(queue,6); 
+    
+    printf("Queue:");
+    PrintQueue(queue);
     printf("\n");
 
-    pop_item_queue(queue);
-    print_queue(queue);
+    Dequeue(queue);
+    Dequeue(queue);
+
+    printf("Queue after 2 dequeues:");
+    PrintQueue(queue);
     printf("\n");
 
     return 0;

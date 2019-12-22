4ca6ca401ec7ddba81de5c493017d263639f4b41
Sebastia Agramunt <sebasvinaros@gmail.com>
2019-12-22 13:30:19 +0100

    :trollface: queue in python

---
+++ queue/queue.py
@@ -0,0 +1,34 @@
+class Queue:
+	def __init__(self):
+		self.queue = []
+
+	def add(self, val):
+		self.queue.append(val)
+
+	def pop(self):
+		if len(self.queue) > 0:
+			val = self.queue[0]
+			del self.queue[0]
+			return val
+		else:
+			return None
+
+
+
+if __name__ == '__main__':
+
+	q = Queue()
+
+	q.add(0)
+	q.add(1)
+	q.add(2)
+	q.add(3)
+	q.add(4)
+
+	print("The queue so far is {}".format(q.queue))
+
+	print("Popping {}".format(q.pop()))
+	print("Popping {}".format(q.pop()))
+	print("Popping {}".format(q.pop()))
+
+	print("The queue is now {}".format(q.queue))

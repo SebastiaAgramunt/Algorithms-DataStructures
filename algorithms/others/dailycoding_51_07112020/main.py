40c2af91225a0f756da5d5f071118f22151ebb99
sebastiaagramunt <sebasvinaros@gmail.com>
2020-11-08 10:28:09 +0100

    another shuffling algo

--- algorithms/others/dailycoding_51_07112020/main.py
+++ algorithms/others/dailycoding_51_07112020/main.py
@@ -10,7 +10,7 @@ from random import randrange, seed
 
 
 import random 
-def shuffle(arr: List[int]) : 
+def shuffle(arr: List[int]) -> None: 
 
     n = len(arr)
     for i in range(n-1,0,-1): 
@@ -18,7 +18,18 @@ def shuffle(arr: List[int]) :
         j = random.randrange(i+1) 
         arr[i],arr[j] = arr[j],arr[i] 
     return arr 
-#Driver code 
+
+# this make take some more time depending on the implementation of
+# array in python (if it is linked list will take longer)
+def shuffle2(arr: List[int]) -> List[int]:
+    new_arr = []
+    while len(arr)>0:
+        i = randrange(len(arr))
+        new_arr.append(arr[i])
+        del arr[i]
+
+    return new_arr
+
 if __name__=='__main__': 
     seed(1)
     a=[0, 1, 2, 3, 4, 5, 6, 7, 8, 
@@ -29,5 +40,8 @@ if __name__=='__main__':
        37, 38, 39, 40, 41, 42, 43,  
        44, 45, 46, 47, 48, 49, 50, 
        51] 
-    shuffle(a) 
-    print(a) 
+    #arr2 = shuffle2(a) 
+    #print(arr2) 
+
+    shuffle(a)
+    print(a)
